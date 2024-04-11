0269bfb9c605622f901508d3870537d3c9311d29
Amit gupta <amitmgupta15@gmail.com>
2024-04-12 00:31:42 +0530

    commit todo add function

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todo } from "../src/model";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
-  const [todo, setTodo] = useState<String>("");
+  const [todo, setTodo] = useState<string>(" ");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
+  // console.log("Todo", todo);
+
+  const handleAddTodo = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+    }
+    setTodo("");
+  };
+  console.log(todos);
   return (
     <div className="App">
       <div className="heading">Taskify</div>
 
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAddTodo={handleAddTodo} />
+      {/* <TodoList /> */}
+
+      {todos.map((item) => (
+        <li>{item.todo}</li>
+      ))}
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,15 +2,22 @@ import React from "react";
 import "./styles.css";
 
 interface Props {
-  todo: String;
-  setTodo: React.Dispatch<React.SetStateAction<String>>;
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAddTodo: (e: React.FormEvent) => void;
 }
 // console.log()
 
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAddTodo }) => {
   return (
-    <form className="input">
-      <input type="input" placeholder="Enter your task" className="inputBox" />
+    <form className="input" onSubmit={handleAddTodo}>
+      <input
+        type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter your task"
+        className="inputBox"
+      />
       <button className="inputAdd" type="submit">
         Add
       </button>
